6d63bf55724573608f9a010709e0582a39d0f188
rpmtravel52-gif <rpmtravel52@gmail.com>
2026-04-25 09:34:42 +0700

    Update packages.ts

--- src/lib/packages.ts
+++ src/lib/packages.ts
@@ -5,10 +5,11 @@ export interface TravelPackage {
 }
 
 export const PACKAGES: TravelPackage[] = [
-  { id: 'curup-palembang',  label: 'Travel Curup – Palembang',  harga: 150000 },
-  { id: 'palembang-curup',  label: 'Travel Palembang – Curup',  harga: 150000 },
-  { id: 'curup-lebong',     label: 'Travel Curup – Lebong',     harga:  80000 },
-  { id: 'lebong-curup',     label: 'Travel Lebong – Curup',     harga:  80000 },
+  { id: 'curup-palembang',  label: 'Travel Curup – Palembang',  harga: 250000 },
+  { id: 'palembang-curup',  label: 'Travel Palembang – Curup',  harga: 250000 },
+  { id: 'curup-lebong',     label: 'Travel Curup – Lebong',     harga:  60000 },
+  { id: 'lebong-curup',     label: 'Travel Lebong – Curup',     harga:  60000 },
+  { id: 'sewa-Mobil-Hiace',  label: 'Sewa Bus Wisata (per hari)',harga: 3000000 },
   { id: 'sewa-bus-wisata',  label: 'Sewa Bus Wisata (per hari)',harga: 3000000 },
 ];
 
